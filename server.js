2bfd602b6dc482092c77fb99a08ab09afd917580
Errollinsjr <errollins.jr@gmail.com>
2021-09-23 17:37:18 -0400

    fix server

--- server.js
+++ server.js
@@ -1,17 +1,28 @@
 const express = require("express");
 const path = require("path");
-const PORT = process.env.PORT || 3001;
 const app = express();
+const mongoose = require("mongoose");
+const routes = require("./routes")
+const PORT = process.env.PORT || 8081;
 
-// Define middleware here
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// Serve up static assets (usually on heroku)
+
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
+//Mongo DB
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+);
+
 // Define API routes here
+app.use(routes);
 
 // Send every other request to the React app
 // Define any API routes before this runs
@@ -20,5 +31,6 @@ app.get("*", (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`🌎 ==> API server now on port ${PORT}!`);
+  console.log(`Port is now live on: ${PORT}!`);
 });
+
